d06a0ac9bad41c7cdfbdeae1d8922a413125bbd9
Louise Reid <l_reid@hotmail.co.uk>
2018-07-10 20:38:50 +0100

    hints rendering score reducing

--- client/src/components/GameCard.js
+++ client/src/components/GameCard.js
@@ -4,7 +4,15 @@ class GameCard extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      roundScore: 5
+      roundScore: 6,
+      hintsShown: []
+    }
+  }
+
+  renderHint(){
+    let hint = (this.props.game.hints).shift()
+    if(!this.state.hintsShown.includes()){
+      this.setState({hintsShown: [...this.state.hintsShown, hint], roundScore: this.state.roundScore -1 })
     }
   }
 
@@ -13,8 +21,13 @@ class GameCard extends React.Component {
   render(){
     if(!this.props.game) return null;
 
-    const { round, location, home_team, away_team, home_goals, away_goals, home_tactics, away_tactics, home_yellow, away_yellow, home_red, away_red, home_possession, away_possession, home_attempts, away_attempts, home_fouls, away_fouls, extra_time, penalties, hint } = this.props.game
+    const { round, location, home_team, away_team, home_goals, away_goals, home_tactics, away_tactics, home_yellow, away_yellow, home_red, away_red, home_possession, away_possession, home_attempts, away_attempts, home_fouls, away_fouls, extra_time, penalties, hints } = this.props.game
 
+    const renderedHints = this.state.hintsShown.map((hint, index) => {
+      return(
+        <li key={index}>{hint}</li>
+      )
+    })
 
     return(
       <React.Fragment>
@@ -45,7 +58,11 @@ class GameCard extends React.Component {
             <p>Red cards: {away_red}</p>
             <p>Tactics: {away_tactics}</p>
           </div>
+          <div className="game-hints">
+            <p>hints: {renderedHints}</p>
+          </div>
         </div>
+        <button onClick={() => this.renderHint()}>Show Hint</button>
       </React.Fragment>
     )
   }
