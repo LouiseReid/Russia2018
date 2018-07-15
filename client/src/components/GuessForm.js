6e2b673ea9e48768c782194b73beca669a9c431a
Louise Reid <l_reid@hotmail.co.uk>
2018-07-15 14:39:27 +0100

    modal opening on giveup

--- client/src/components/GuessForm.js
+++ client/src/components/GuessForm.js
@@ -1,49 +1,78 @@
 import React from 'react';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root')
 
 class GuessForm extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       home: '',
-      away:''    }
+      away: '',
+      modalIsOpen: false
+    }
   }
 
-  handleGuess(){
-    if(this.props.home === this.state.home && this.props.away === this.state.away){
+  handleGuess() {
+    if (this.props.home === this.state.home && this.props.away === this.state.away) {
       this.props.correctGuess()
       this.setState({home: '', away: '', incorrectGuessCount: 0})
-    } else if (this.props.home === this.state.home && this.props.away !== this.state.away){
-      this.setState({away: '', incorrectGuessCount: this.state.incorrectGuessCount + 1})
+    } else if (this.props.home === this.state.home && this.props.away !== this.state.away) {
+      this.setState({
+        away: '',
+        incorrectGuessCount: this.state.incorrectGuessCount + 1
+      })
       this.props.handleGuess()
     } else if (this.props.home !== this.state.home && this.props.away === this.state.away) {
-      this.setState({home: '', incorrectGuessCount: this.state.incorrectGuessCount + 1})
+      this.setState({
+        home: '',
+        incorrectGuessCount: this.state.incorrectGuessCount + 1
+      })
       this.props.handleGuess()
     } else {
-      this.setState({home: '', away: '', incorrectGuessCount: this.state.incorrectGuessCount + 1})
+      this.setState({
+        home: '',
+        away: '',
+        incorrectGuessCount: this.state.incorrectGuessCount + 1
+      })
       this.props.handleGuess()
     }
   }
 
-  giveUp(){
+  giveUp() {
+    this.openModal()
     this.props.giveUp()
     this.setState({home: '', away: '', incorrectGuessCount: 0})
   }
 
-  render(){
+  openModal() {
+    this.setState({modalIsOpen: true})
+  }
 
-    return(
-      <div className="guess-inputs">
+  afterOpenModal() {
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <div className="guess-inputs">
         <input
           type="text"
           placeholder="home team"
           value={this.state.home}
-          onChange={event => this.setState({ home: event.target.value })}
+          onChange={event => this.setState({home: event.target.value})}
         />
         <input
           type="text"
           placeholder="away team"
           value={this.state.away}
-          onChange={event => this.setState({ away: event.target.value })}
+          onChange={event => this.setState({away: event.target.value})}
         />
         <button
           className="btn btn-guess"
@@ -58,9 +87,32 @@ class GuessForm extends React.Component {
           Give Up
         </button>
       </div>
+      <Modal
+        isOpen={this.state.modalIsOpen}
+        onAfterOpen={() => this.afterOpenModal}
+        onRequestClose={() => this.closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        >
+        <div className="modal-content">
+          <button onClick={() => this.closeModal()}>x</button>
+          <p>Correct Game: {this.props.home} vs {this.props.away} </p>
+        </div>
+      </Modal>
+      </React.Fragment>
     )
   }
-
 }
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
 
 export default GuessForm
